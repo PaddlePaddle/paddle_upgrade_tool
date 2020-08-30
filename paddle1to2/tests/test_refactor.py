68e52a89a4868eaaa5c5f16b4b117a658a2c848e
Shibo Tao <taoshibo@baidu.com>
2020-08-30 10:58:03 +0800

    add unittest for refactor_import and norm_api_alias

--- paddle1to2/tests/test_refactor.py
+++ paddle1to2/tests/test_refactor.py
@@ -4,10 +4,8 @@ import unittest
 import textwrap
 from tempfile import NamedTemporaryFile
 
-sys.path.insert(0, '')
-
-from paddle1to2.refactor import * 
 from bowler import Query
+from paddle1to2.refactor import * 
 
 def _refactor_helper(refactor_func, input_src, change_spec) -> str:
     try:
@@ -26,7 +24,7 @@ def _refactor_helper(refactor_func, input_src, change_spec) -> str:
 class TestRefactorDemo(unittest.TestCase):
     change_spec = {}
 
-    def test_rename_1(self):
+    def test_refactor_demo(self):
         input_src = '''
         old_api()
         '''
@@ -38,6 +36,134 @@ class TestRefactorDemo(unittest.TestCase):
         output_src = _refactor_helper(refactor_demo, input_src, self.change_spec)
         self.assertEqual(output_src, expected_src)
 
+
+class TestRefactorImport(unittest.TestCase):
+    def _run(self, change_spec, input_src, expected_src):
+        input_src = textwrap.dedent(input_src).strip() + '\n'
+        expected_src = textwrap.dedent(expected_src).strip() + '\n'
+        output_src = _refactor_helper(refactor_import, input_src, change_spec)
+        self.assertEqual(output_src, expected_src)
+
+    def test_refactor_import(self):
+        input_src = '''
+        import paddle
+        '''
+        expected_src = '''
+        import paddle
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        import paddle.fluid as fluid
+        '''
+        expected_src = '''
+        import paddle
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        import paddle
+        import paddle.fluid as fluid
+        '''
+        expected_src = '''
+        import paddle
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        import paddle
+        import paddle.fluid as fluid
+        '''
+        expected_src = '''
+        import paddle
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        import paddle
+        import paddle.fluid as fluid
+        fluid.api()
+
+        def func():
+            fluid.api()
+        '''
+        expected_src = '''
+        import paddle
+        paddle.fluid.api()
+
+        def func():
+            paddle.fluid.api()
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        import paddle.fluid as fluid
+        fluid.api()
+
+        def func():
+            fluid.api()
+        '''
+        expected_src = '''
+        import paddle
+        paddle.fluid.api()
+
+        def func():
+            paddle.fluid.api()
+        '''
+        self._run({}, input_src, expected_src)
+        #--------------
+        input_src = '''
+        from paddle.fluid.layers import Layer
+
+        class CustomLayer(Layer):
+            pass
+        print(Layer.__name__)
+        print(type(Layer))
+        '''
+        expected_src = '''
+        import paddle
+
+        class CustomLayer(paddle.fluid.layers.Layer):
+            pass
+        print(paddle.fluid.layers.Layer.__name__)
+        print(type(paddle.fluid.layers.Layer))
+        '''
+        self._run({}, input_src, expected_src)
+
+
+class TestNormApiAlias(unittest.TestCase):
+    change_spec = {
+            "paddle.fluid.Layer": {
+                "alias": [
+                    "paddle.fluid.layers.Layer",
+                    "paddle.fluid.layers1.layers2.Layer",
+                    ]
+                }
+            }
+
+    def _run(self, change_spec, input_src, expected_src):
+        input_src = textwrap.dedent(input_src).strip() + '\n'
+        expected_src = textwrap.dedent(expected_src).strip() + '\n'
+        output_src = _refactor_helper(norm_api_alias, input_src, change_spec)
+        self.assertEqual(output_src, expected_src)
+
+    def test_norm_api_alias(self):
+        input_src = '''
+        import paddle
+
+        layer = paddle.fluid.Layer()
+        layer = paddle.fluid.layers.Layer()
+        layer = paddle.fluid.layers1.layers2.Layer()
+        '''
+        expected_src = '''
+        import paddle
+
+        layer = paddle.fluid.Layer()
+        layer = paddle.fluid.Layer()
+        layer = paddle.fluid.Layer()
+        '''
+        self._run(self.change_spec, input_src, expected_src)
+ 
 
 if __name__ == '__main__':
     unittest.main()
